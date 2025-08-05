369d24be60d611d734a6e11dda5ce0356f125439
Kareem42 <kareem4209@gmail.com>
2025-08-05 12:08:52 -0500

    mongodb changes

--- Server_Setup/server/db.js
+++ Server_Setup/server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:tzMWOIkvxz229eOi@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:UjACn0syQd35NAv2@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
